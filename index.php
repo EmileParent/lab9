2325d49be41d52f98d34f23e73c373af36d93b3f
Emile Parent Corriveau <parentcoe@gmail.com>
2021-05-04 17:58:31 -0400

    ajout image fond

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+// Silence is golden.
+/*
+https://www.youtube.com/watch?v=gqKaZO9epHM
+
+extension: php intelephense
+php docBlocker
+wordpress snippet
+gitlens
+scss intelliSence
+path Intellisense
+auto rename tag
+polacode (photo du code)
+GitHub Theme
+vscode-icons
+
+extension wp
+wpscan
+
+headless wp frameworks
+frontity
+Gatsby
+NexJS
+Unit testing frame work
+phpUnit
+page Builders
+AWS (virtual drop late)
+Docker
+
+wp plugin
+
+starter templates
+
+
+
+
+
+
+*/
